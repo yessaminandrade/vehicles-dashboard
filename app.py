d92c1bbbd4c2d15ef0d52bd93bd87a956c58afac
yessamin <136747651+yessaminandrade@users.noreply.github.com>
2026-04-22 18:48:21 -0600

    simplify streamlit config

--- app.py
+++ app.py
@@ -2,23 +2,18 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-import os
-import streamlit as st
-
 st.set_page_config(layout="wide")
 
-df = pd.read_csv('vehicles_us.csv')
+df = pd.read_csv("vehicles_us.csv")
+df = df[df["odometer"] < 300000]
+df = df[df["price"] < 100000]
 
-# limpiar datos extremos
-df = df[df['odometer'] < 300000]
-df = df[df['price'] < 100000]
+st.header("Dashboard de vehículos")
 
-st.header('Dashboard de vehículos')
-
-if st.button('Construir histograma'):
-    fig = px.histogram(df, x='odometer')
+if st.button("Construir histograma"):
+    fig = px.histogram(df, x="odometer")
     st.plotly_chart(fig)
 
-if st.button('Construir dispersión'):
-    fig = px.scatter(df, x='odometer', y='price')
+if st.button("Construir dispersión"):
+    fig = px.scatter(df, x="odometer", y="price")
     st.plotly_chart(fig)
